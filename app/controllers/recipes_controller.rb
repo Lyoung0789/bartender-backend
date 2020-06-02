ccc8d1925f1eae46800485ef0d1cb6736e949748
Lyoung0789 <Lestermyoung07@gmail.com>
2020-06-02 18:47:46 -0400

    Add Create functionality and pry gem

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,4 +9,21 @@ class RecipesController < ApplicationController
         @recipe = Recipe.find_by(id: params[:id])
         render json: @recipe 
     end 
+
+    def create 
+        # byebug
+        @recipe = Recipe.create(recipe_params)
+        # byebug
+        render json: @recipe
+    end 
+
+
+
+
+    private 
+    
+    def recipe_params
+        params.require(:recipe).permit(:title, :instructions, :liquor)
+    end 
+
 end
