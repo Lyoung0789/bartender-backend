f1c500a059742795feeb1ca5038a43e837e1cd9f
Lyoung0789 <Lestermyoung07@gmail.com>
2020-06-06 00:59:52 -0400

    Changed the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Recipe.create(title: "Old Fashion", instructions:"2 oz of Whiskey/Bourbon, 2 dashes of Bitters, Orange peel twist, 1/2 tsp of Sugar, stir over ice", liquor: "Buffalo Trace")
+Recipe.create(title: "Old Fashion", instructions:"2 oz of Whiskey/Bourbon, 2 dashes of Bitters, Orange peel twist, 1/2 tsp of Sugar, stir over ice", liquor: "Whiskey")
 Review.create(name: "Mike", content: "This was delicious! I would add a splash of club soda to give the drink a little more volume, and not make it as strong.", recipe_id: 1)
 
 Recipe.create(title: "Mule", instructions: "2 oz vodka, 1 oz lime juice, 1 oz simple syrup, 2 oz ginger beer, garnish with lime wedge", liquor: "Vodka")
